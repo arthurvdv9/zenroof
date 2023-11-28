6cf880ccdfd97c2142664d4d961557a0fee2e222
Vasco Pereira <happen1337.ltd@gmail.com>
2023-11-28 15:50:51 +0000

    controller-index

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,41 @@
+class TicketsController < ApplicationController
+  before_action :set_ticket, only: [:show]
+
+  def index
+    @tickets = Ticket.all
+  end
+
+  def show
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    @ticket.user = current_user
+    if @ticket.save
+      #redirect_to path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
+    # redirect_to path
+  end
+
+  private
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :description, :status, :priority)
+  end
+
+end
