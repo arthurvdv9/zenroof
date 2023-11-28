3dc79bd731c4fa3aaa138ac75356038594cea133
Arthur Van de Venne <arthur.vandevenne@gmail.com>
2023-11-28 16:03:06 +0000

    model associations & dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    # @houses = House.where(user_id: current_user)
+    # @tickets = Ticket.where(user_id: current_user)
+  end
 end
