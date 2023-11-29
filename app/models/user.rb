e53e2ed185f1aecdfd435f48bd357176de819e8a
Vasco Pereira <happen1337.ltd@gmail.com>
2023-11-29 11:33:21 +0000

    uder validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates  :first_name, presence: true
-  validates  :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
