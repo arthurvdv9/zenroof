8b1ef9e9a431ed2ffaf68366a015c3e185a60c8e
Vasco Pereira <happen1337.ltd@gmail.com>
2023-11-29 11:27:37 +0000

    room validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
+  validates :name, presence: true
+  validates :number, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..100 }
+
   belongs_to :user
   belongs_to :house
   has_many :tickets
