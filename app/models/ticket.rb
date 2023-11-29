341a630daa7add0babe12095ba0dcf94103bcaa8
Vasco Pereira <happen1337.ltd@gmail.com>
2023-11-29 11:32:34 +0000

    ticket validations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,10 @@
 class Ticket < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :status, presence: true
+  validates :priority, presence: true
+
+  belongs_to :user
   belongs_to :rooms
   has_many_attached :photos
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates  :first_name, presence: true
+  validates  :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many  :houses
