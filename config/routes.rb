444bdc489d4022cdffd2b60da38de4b6c8b3e65c
Arthur Van de Venne <arthur.vandevenne@gmail.com>
2023-11-30 15:35:31 +0000

    jsjsj

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#dashboard"
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/dashboard", to: "pages#dashboard", as: "dashboard"
