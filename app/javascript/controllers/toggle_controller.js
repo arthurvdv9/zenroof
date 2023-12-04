e6b85b7f4471094cbc469c709c3682a8baf8772e
Mariana Miragaia <miragaia.mariana@gmail.com>
2023-12-04 15:29:03 +0000

    add ons for tenant dashboard border header

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,13 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="new-ticket-form"
 export default class extends Controller {
-  static targets = ["togglableElement"]
+  static targets = ["togglableElement","button","cancelbutton"]
 
   connect() {
     console.log("Hello from toggle_controller.js")
   }
 
   fire() {
+    // console.log(cancelbuttonTarget);
     this.togglableElementTarget.classList.toggle("d-none");
+    this.buttonTarget.classList.toggle("d-none")
   }
 }
