34f98341a50894c03bde2133217eb3002bfc8e02
joana faria <paisdefaria.joana@gmail.com>
2023-12-07 11:00:10 +0000

    Fixed house tickets notification and button to change status of Ticket , 07-12 Working JM stamp of approval

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
-
   def home
   end
 
   def dashboard
     @houses = House.where(user_id: current_user)
-
     @sorted_houses = @houses.sort_by { |house| house.rooms.each { |room| room.tickets.count } }.reverse
-
 
     if params[:query].present?
       sql_subquery = "name ILIKE :query OR address ILIKE :query"
@@ -37,7 +34,6 @@ class PagesController < ApplicationController
     # puts @houses
   end
 
-
   def tenanthistory
     @tickets = current_user.room.present? ? Ticket.where(room_id: current_user.room.id) : []
     if params[:query].present?
@@ -47,15 +43,14 @@ class PagesController < ApplicationController
     @room = current_user.room
   end
 
-  def tenanthistory
-    @tickets = current_user.room.present? ? Ticket.where(room_id: current_user.room.id) : []
-    @room = current_user.room
-  end
+  # def tenanthistory
+  #   @tickets = current_user.room.present? ? Ticket.where(room_id: current_user.room.id) : []
+  #   @room = current_user.room
+  # end
 
   def ownerhistory
     @house = House.find(params[:house_id])
     @rooms = Room.where(house_id: @house.id)
     @tickets = @rooms.map { |room| room.tickets }.flatten
   end
-
 end
